bb282977706e7b5dc977f5ab13d4693e058201e8
Radgus <dominguezradgus@gmail.com>
2022-07-22 00:10:49 -0500

    Estructura del header terminada, falta funcionalidad

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import colors from '../../resource/colors/index';
+import LoginContainer from './components/LoginContainer';
+import logo from '../../resource/images/macropay-logo.png';
+import cartIcon from '../../resource/images/shopping-icon.png';
+import CircularDiv from './components/CircularDiv';
 
+
+const LogoContainer = styled.div`
+  background-color: ${colors.third_bg};
+  height: 7rem;
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr;
+  align-items: center;
+  justify-items: center;
+`;
+
+const OptionsContainer = styled.div`
+  padding: 0 1rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  color: ${colors.secondary_text};
+  background-color: ${colors.third_bg};
+  border-top: 0.1rem solid ${colors.secondary_bg};
+`;
+
+const H4 = styled.h4`
+  margin: 0 1.5rem;
+  cursor: pointer;
+  :hover {
+    color: ${colors.fourth_text};
+  }
+  font-size: 1.2rem;
+`;
 
 const Header = () => {
 
   return (
-    <h1>Header</h1>
+    <>
+    <LogoContainer>
+      <img src={logo} alt="logo macropay" width='90px'/>
+      <CircularDiv>
+        <img src={cartIcon} alt="logo macropay" width='30px'/>
+      </CircularDiv>
+      <LoginContainer/>
+    </LogoContainer>
+    <OptionsContainer>
+      <H4>Products</H4>
+      <H4>About</H4>
+      <H4>Contact</H4>
+    </OptionsContainer>
+
+    </>
   )
 }
 
---
+++ src/resource/colors/index.js
@@ -0,0 +1,12 @@
+const colors = {
+  'primary_bg': 'whitesmoke',
+  'secondary_bg': '#fff',
+  'third_bg': '#0047ba',    // azul
+  'fourth_bg': '#ffe900',   // amarilla
+  'primary_text': 'black',
+  'secondary_text': '#fff',
+  'third_text': '#0047ba', 
+  'fourth_text': '#ffe900',
+}
+
+export default colors;

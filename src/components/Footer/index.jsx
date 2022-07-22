d124fb8d010092c55d3f9bad7f3dcec28379f894
Radgus <dominguezradgus@gmail.com>
2022-07-22 01:04:19 -0500

    seguimos con el footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import colors from '../../resource/colors';
+
+
+const Container = styled.div`
+  background-color: ${colors.fourth_bg};
+  height: 10rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 
 const Footer = () => {
 
   return (
-    <h1>Footer</h1>
+    <Container>Footer</Container>
   )
 }
 
